63468c6fc6e17c1d8fde3591c084023a9902340c
Baxtigul <jumabekova.baxtigul@gmail.com>
2022-09-21 20:50:54 +0500

    a

--- src/main/java/school/mjc/stage0/loops/task2/FactorialNumbers.java
+++ src/main/java/school/mjc/stage0/loops/task2/FactorialNumbers.java
@@ -1,7 +1,20 @@
-package school.mjc.stage0.loops.task2;
+18 lines (16 sloc)  422 Bytes
+
+        package school.mjc.stage0.loops.task2;
 
 public class FactorialNumbers {
     public void printFactorialRow(int printToInclusive) {
 
+        if(printToInclusive>0){
+            int fact = 1;
+            int i = 1;
+            System.out.println(1);
+            while(i<=printToInclusive)
+            {
+                fact = fact * i;
+                i++;
+                System.out.println(fact);
+            }}else
+            System.out.println(1);
     }
 }
